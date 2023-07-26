ab2fa03f11333e3921372c8145ed44f57791b16b
alasawtsoh <janico_coronel@yahoo.com>
2023-07-26 18:26:07 +0800

    html css js

--- app.js
+++ app.js
@@ -1,168 +1,159 @@
-const cells = document.querySelectorAll("[data-cell]");
-const historyLog = document.querySelector(".history-log");
+let board = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let moveHistory = [];
+let moveHistoryIndex = -1;
+let scoreX = 0;
+let scoreO = 0;
+let scoreDraw = 0;
 
-let currentPlayer = "X";
-let gameBoard = ["", "", "", "", "", "", "", "", ""];
-let history = [];
-let currentMoveIndex = -1;
-let inHistoryMode = false; // Flag to indicate whether in history navigation mode
+const cells = document.querySelectorAll('.cell');
+const undoButton = document.getElementById('undoButton');
+const redoButton = document.getElementById('redoButton');
+const restartButton = document.getElementById('restartButton');
+const scoreXElement = document.getElementById('scoreX');
+const scoreOElement = document.getElementById('scoreO');
+const scoreDrawElement = document.getElementById('scoreDraw');
+const historyLog = document.getElementById('historyLog');
+const clearHistoryButton = document.getElementById('clearHistoryButton');
 
-function checkWin() {
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
 
-  for (const pattern of winPatterns) {
-    const [a, b, c] = pattern;
-    if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-      return true;
+cells.forEach((cell, index) => {
+    cell.addEventListener('click', () => makeMove(index));
+  });
+
+  undoButton.addEventListener('click', undoMove);
+  redoButton.addEventListener('click', redoMove);
+  restartButton.addEventListener('click', resetBoard);
+  clearHistoryButton.addEventListener('click', clearHistory);
+  
+
+
+  function checkWinner() {
+    // Define all the possible winning combinations
+    const winCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+      [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+  
+
+  // Check each winning combination to see if there's a winner
+  for (const combination of winCombinations) {
+    const [a, b, c] = combination;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a]; // Return the winner ('X' or 'O')
     }
   }
 
-  return false;
+  return null; // No winner yet
 }
 
 function checkDraw() {
-  return gameBoard.every((cell) => cell);
+  // Check if all cells are filled and no winner
+  return board.every(cell => cell !== '') && !checkWinner();
 }
 
-function handleNormalClick(index) {
-    if (currentMoveIndex < history.length - 1) {
-      history.splice(currentMoveIndex + 1);
-    }
+function makeMove(index) {
+    if (board[index] === '' && !checkWinner() && !checkDraw()) {
+      board[index] = currentPlayer;
+      moveHistory.push(index);
+      moveHistoryIndex = moveHistory.length - 1;
+      cells[index].innerText = currentPlayer;
   
-    if (gameBoard[index] === "" && !checkWin() && !checkDraw()) {
-      gameBoard[index] = currentPlayer;
-      const move = { player: currentPlayer, index: index };
-      history.push(move);
-  
-      currentMoveIndex = history.length - 1;
-      renderBoard();
-      currentPlayer = currentPlayer === "X" ? "O" : "X";
-  
-      if (checkWin()) {
+      const winner = checkWinner();
+      if (winner) {
+        updateScores(winner);
         setTimeout(() => {
-          alert(`${move.player} wins!`);
-        }, 100);
+          alert(winner === 'X' ? 'Player X wins!' : 'Player O wins!');
+        }, 10);
       } else if (checkDraw()) {
+        updateScores('Draw');
         setTimeout(() => {
-          alert("It's a draw!");
-        }, 100);
+          alert('It\'s a draw!');
+        }, 10);
+      }
+  
+      // Update currentPlayer after checking for winner or draw
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  
+      // Add the move to the history log
+      historyLog.innerHTML += `<li>Player ${currentPlayer} moved to cell ${index}</li>`;
+      historyLog.scrollTop = historyLog.scrollHeight; // Auto-scroll to the bottom of the log
+    }
+  }
+
+  function updateScores(winner) {
+  if (winner === 'X') {
+    scoreX++;
+    if (scoreXElement) {
+      scoreXElement.innerText = scoreX;
+    }
+  } else if (winner === 'O') {
+    scoreO++;
+    if (scoreOElement) {
+      scoreOElement.innerText = scoreO;
+    }
+  } else if (winner === 'Draw') {
+    scoreDraw++;
+    if (scoreDrawElement) {
+      scoreDrawElement.innerText = scoreDraw;
+    }
+  }
+}
+
+// Initial scores display
+if (scoreXElement) {
+    scoreXElement.innerText = scoreX;
+  }
+  if (scoreOElement) {
+    scoreOElement.innerText = scoreO;
+  }
+  if (scoreDrawElement) {
+    scoreDrawElement.innerText = scoreDraw;
+  }
+
+
+  function undoMove() {
+    if (moveHistoryIndex >= 0) {
+      const index = moveHistory[moveHistoryIndex];
+      board[index] = '';
+      cells[index].innerText = '';
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      moveHistoryIndex--;
+  
+      // Remove the corresponding entry from the history log
+      if (moveHistory.length > 0) {
+        historyLog.removeChild(historyLog.children[moveHistoryIndex + 1]);
       }
     }
   }
-  
-  function handleClick(index) {
-    if (inHistoryMode) {
-      // Handle click during history navigation
-      exitHistoryMode(); // Exit history mode to enable normal clicks
-      handleNormalClick(index); // Perform the normal click
-    } else {
-      handleNormalClick(index); // Handle normal click
-    }
-  }
-  
-  
-  function renderBoard() {
-    cells.forEach((cell, index) => {
-      cell.textContent = gameBoard[index];
-    });
-    
 
-  // Update history log
-  historyLog.innerHTML = "";
-  for (const move of history) {
-    const { player, index } = move;
-    const moveItem = document.createElement("div");
-    moveItem.textContent = `${player} placed at position ${index}`;
-    historyLog.appendChild(moveItem);
-  }
-}
 
-cells.forEach((cell, index) => {
-    cell.addEventListener("click", () => handleClick(index));
-  });
+  function redoMove() {
+    if (moveHistoryIndex < moveHistory.length - 1) {
+      moveHistoryIndex++;
+      const index = moveHistory[moveHistoryIndex];
+      board[index] = currentPlayer;
+      cells[index].innerText = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
   
-  renderBoard();
-  
-  function showPrevious() {
-    if (currentMoveIndex > 0) {
-      inHistoryMode = true;
-      currentMoveIndex--;
-      const move = history[currentMoveIndex];
-      gameBoard = history.slice(0, currentMoveIndex + 1).map((move) => (move.player === "X" ? "X" : "O"));
-      currentPlayer = move.player === "X" ? "O" : "X";
-      renderBoard();
-      exitHistoryMode(); // Call exitHistoryMode to reattach event listeners
+      // Add the redo move entry to the history log
+      historyLog.innerHTML += `<li>Player ${currentPlayer} moved to cell ${index}</li>`;
+      historyLog.scrollTop = historyLog.scrollHeight; // Auto-scroll to the bottom of the log
     }
   }
 
- function showNext() {
-  if (currentMoveIndex < history.length - 1) {
-    inHistoryMode = true;
-    currentMoveIndex++;
-    const move = history[currentMoveIndex];
-    gameBoard[move.index] = move.player;
-    currentPlayer = move.player === "X" ? "O" : "X";
-    renderBoard();
-    exitHistoryMode(); // Call exitHistoryMode to reattach event listeners
-  }
+
+function resetBoard() {
+  board = ['', '', '', '', '', '', '', '', ''];
+  currentPlayer = 'X';
+  moveHistory = [];
+  moveHistoryIndex = -1;
+  cells.forEach(cell => cell.innerText = '');
+  historyLog.innerHTML = '';
 }
-  
 
-  function removeCellEventListeners() {
-    cells.forEach((cell) => {
-      cell.removeEventListener("click", handleNormalClick);
-    });
+function clearHistory() {
+    // Clear the history log
+    historyLog.innerHTML = '';
   }
-
- function exitHistoryMode() {
-  inHistoryMode = false;
-  removeCellEventListeners(); // Remove cell event listeners during history navigation
-  renderBoard(); // Update the board after exiting history mode
-}
-  
-  
-  function resetGame() {
-    gameBoard = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = "X";
-    history = [];
-    currentMoveIndex = -1; // Reset the move index
-    renderBoard();
-    addCellEventListeners(); // Add cell event listeners after resetting the game
-  }
-  
-  function addCellEventListeners() {
-    cells.forEach((cell, index) => {
-      cell.addEventListener("click", () => {
-        if (!inHistoryMode) {
-          handleNormalClick(index);
-        }
-      });
-    });
-  }
-  
-  addCellEventListeners();
-  
-  function replayGame() {
-    resetGame();
-  } 
-  
-  
-  let playerXScore = 0;
-let playerOScore = 0;
-
-function updateScoreboard() {
-  const playerXScoreElement = document.getElementById("player-x-score");
-  const playerOScoreElement = document.getElementById("player-o-score");
-
-  playerXScoreElement.textContent = `Player X: ${playerXScore}`;
-  playerOScoreElement.textContent = `Player O: ${playerOScore}`;
-}
